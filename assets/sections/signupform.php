3de28cae1cce6ac6bc97673a80593c98065bf968
alvin-muriithi <alvin.muriithi23@strathmore.edu>
2025-06-10 22:49:10 +0300

    Sign up form added

---
+++ assets/sections/signupform.php
@@ -0,0 +1,20 @@
+<div class="modal" id="signup">
+    <div class="modal-content">
+        <span class="close">&times;</span>
+        <h2>Create Account</h2>
+        <p>User authentication is handled via Firebase. [Placeholder: Insert Firebase API integration here.]</p>
+        <form id="signup-form">
+            <div class="form-group">
+                <input type="text" placeholder="Full Name" required>
+            </div>
+            <div class="form-group">
+                <input type="email" placeholder="Email Address" required>
+            </div>
+            <div class="form-group">
+                <input type="password" placeholder="Password" required>
+            </div>
+            <button type="submit" class="cta-button">Sign Up</button>
+            <p>Already have an account? <a href="#login">Login</a></p>
+        </form>
+    </div>
+</div>
