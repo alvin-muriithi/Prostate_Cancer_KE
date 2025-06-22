e94e825b5043e430a7964d025d23cb66e5ff4d46
alvin-muriithi <alvin.muriithi23@strathmore.edu>
2025-06-22 23:13:34 +0300

    newsletter backend

---
+++ newsletter.php
@@ -0,0 +1,47 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/phpmailer/phpmailer/src/Exception.php';
+require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require 'vendor/phpmailer/phpmailer/src/SMTP.php';
+
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['subscriber_email'])) {
+    $email = filter_var(trim($_POST['subscriber_email']), FILTER_SANITIZE_EMAIL);
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "invalid";
+        exit();
+    }
+
+    $mail = new PHPMailer(true);
+
+    try {
+        // SMTP Setup (use your credentials)
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com'; 
+        $mail->SMTPAuth = true;
+        $mail->Username = 'muriithialvin06@gmail.com';
+        $mail->Password = 'ioxv pffb wgdv sotd';
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+
+        // Message
+        $mail->clearAddresses(); // clear previous address
+        $mail->addAddress($email); // send to subscriber now
+        $mail->Subject = 'Welcome to ProstateCare Newsletter!';
+
+        // Load HTML template content
+        $newsletterTemplate = file_get_contents("newsletter-template.html");
+
+        $mail->isHTML(true);
+        $mail->Body = $newsletterTemplate;
+
+        $mail->send();
+        echo "success";
+
+    } catch (Exception $e) {
+        echo "error";
+    }
+}
+?>
