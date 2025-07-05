26796462a62c8490eafe7fad7ccc7e2e938912f3
alvin-muriithi <122809275+alvinmurii@users.noreply.github.com>
2025-07-05 20:04:23 +0300

    swiping

---
+++ assets/js/swiper.js
@@ -0,0 +1,24 @@
+
+
+  const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1
+      },
+      768: {
+        slidesPerView: 2
+      },
+      1024: {
+        slidesPerView: 3
+      }
+    }
+  });
+  
+
