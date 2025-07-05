1ee81941df7ac5914dc3c2ec4ff1c9c6da9212a7
alvin-muriithi <122809275+alvinmurii@users.noreply.github.com>
2025-07-05 20:05:21 +0300

    gen

---
+++ assets/js/config.js
@@ -0,0 +1,13 @@
+// Tailwind config
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: { primary: "#40E0D0", secondary: "#87CEEB" },
+      borderRadius: {
+        none: "0px", sm: "4px", DEFAULT: "8px", md: "12px", lg: "16px",
+        xl: "20px", "2xl": "24px", "3xl": "32px", full: "9999px", button: "8px"
+      },
+    },
+  },
+};
+
