fab8371c8f7db8f2a0862243e23941ecfb0517dd
alvin-muriithi <122809275+alvinmurii@users.noreply.github.com>
2025-07-07 14:19:03 +0300

    gen

--- prostatecare_kenya.sql
+++ prostatecare_kenya.sql
@@ -92,3 +92,13 @@ ALTER TABLE users ADD COLUMN provider VARCHAR(50) DEFAULT 'email';
 
 use database prostatecare_kenya;
 select * from users;
+
+
+-- Create the admins table
+CREATE TABLE admins (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  full_name VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
